d26185e3e6e2407fbf715e11d76939c66313f999
Colin Warner <colin.x.warner@gmail.com>
2021-05-14 12:18:12 -0500

    init

---
+++ src/main/java/com/capstone/gogreen/GogreenApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.gogreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogreenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GogreenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capstone/gogreen/GogreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.gogreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GogreenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
