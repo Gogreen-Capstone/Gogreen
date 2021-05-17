d8d4df13fcda0a771c7d66d81b2284d1add2ca5e
Colin Warner <colin.x.warner@gmail.com>
2021-05-17 14:05:30 -0500

    Incorporating updates to staging into current home page code

--- src/main/java/com/capstone/gogreen/controllers/HomeController.java
+++ src/main/java/com/capstone/gogreen/controllers/HomeController.java
@@ -1,4 +1,17 @@
 package com.capstone.gogreen.controllers;
 
+import com.capstone.gogreen.models.User;
+import com.capstone.gogreen.repositories.UserRepository;
+import com.capstone.gogreen.services.UserDetailsLoader;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Controller
 public class HomeController {
+
 }
--- src/main/java/com/capstone/gogreen/services/UserDetailsLoader.java
+++ src/main/java/com/capstone/gogreen/services/UserDetailsLoader.java
@@ -1,4 +1,30 @@
 package com.capstone.gogreen.services;
 
-public class UserDetailsLoader {
+import com.capstone.gogreen.models.User;
+import com.capstone.gogreen.models.UserWithRoles;
+import com.capstone.gogreen.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsLoader implements UserDetailsService {
+
+    private final UserRepository usersDao;
+
+
+    public UserDetailsLoader(UserRepository usersDao) {
+        this.usersDao = usersDao;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = usersDao.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("No user found for " + username);
+        }
+
+        return new UserWithRoles(user);
+    }
 }
