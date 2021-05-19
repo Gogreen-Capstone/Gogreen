c822ad24b649ccddc72bc6b688dbdfa4b9bfa8bd
Yasin Mogultay <yasin.mglty@gmail.com>
2021-05-19 09:57:45 -0500

    feat:Completed user ability to update their username email and password with unique validation check

--- src/main/java/com/capstone/gogreen/controllers/UserController.java
+++ src/main/java/com/capstone/gogreen/controllers/UserController.java
@@ -24,38 +24,69 @@ public class UserController {
     }
 
     @GetMapping("/dashboard")
-    public String showUserDashboard(Model model){
+    public String showUserDashboard(Model model) {
         User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("user", loggedInUser);
         return "users/dashboard";
     }
 
+    //Edit user information getMapping
     @GetMapping("/edit-user")
-    public String showEditUser(Model model){
+    public String showEditUser(Model model) {
         User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal(); //Getting logged in user
         usersDao.getOne(loggedInUser.getId()); //getting that user by id of logged in user
         model.addAttribute("user", usersDao.getOne(loggedInUser.getId())); //adding that user object
         return "users/edit-user";
     }
 
+    //Edit user information postMapping
     @PostMapping("/edit-user")
-    public String editUser(@Valid @ModelAttribute User user,
-                           @RequestParam(name="password")String password,
-                           @RequestParam(name = "username")String username,
-                           @RequestParam(name = "email")String email,
-                           Errors validation){
+    public String editUser(@Valid @ModelAttribute User user, //Valid attribute is for validation
+                           @RequestParam(name = "password") String password,
+                           @RequestParam(name = "username") String username,
+                           @RequestParam(name = "email") String email,
+                           @RequestParam(name = "confirm") String confirm,
+                           Errors validation,
+                           Model model) { //errors validation also needed for validation
         User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal(); //Getting logged in user
         User userId = usersDao.getOne(loggedInUser.getId());
         String hash = passwordEncoder.encode(password);
 
+        // checking to make sure password and confirm password match
+        if (!user.getPassword().equals(confirm)) {
+            validation.rejectValue(
+                    "password",
+                    "user.password",
+                    "Passwords do not match"
+            );
+        }
+
+        //Check if username and email is match with some other records from our database
+        if (validation.hasErrors()) {
+            model.addAttribute("errors", validation);
+            model.addAttribute("user", user);
+            return "users/edit-user";
+        } else if (usersDao.findByUsername(user.getUsername()) != null && usersDao.findByEmail(user.getEmail()) != null) {
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
+            return "users/edit-user";
+        } else if (usersDao.findByUsername(user.getUsername()) != null) {
+            model.addAttribute("username", user.getUsername());
+            return "users/edit-user";
+        } else if (usersDao.findByEmail(user.getEmail()) != null) {
+            model.addAttribute("email", user.getEmail());
+            return "users/edit-user";
+        }
+
+        //setting the username email and password
         userId.setPassword(hash);
         userId.setUsername(username);
         userId.setEmail(email);
+
+        //saving those changes to the user
         usersDao.save(userId);
-        return "users/edit-user";
+        return "redirect:/dashboard";
     }
-
-
 
 
 }
