dad1ad781d27515889519243514df3f145eb2309
Yasin Mogultay <yasin.mglty@gmail.com>
2021-05-25 15:49:59 -0500

    feat:added Mapbox api and getting users locations and viewing into map as a pin and pop ups

--- src/main/java/com/capstone/gogreen/controllers/LocationController.java
+++ src/main/java/com/capstone/gogreen/controllers/LocationController.java
@@ -3,9 +3,10 @@ package com.capstone.gogreen.controllers;
 import com.capstone.gogreen.models.Location;
 import com.capstone.gogreen.repositories.LocationRepository;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,29 +15,23 @@ public class LocationController {
 
     private final LocationRepository locationsDao;
 
-
     public LocationController(LocationRepository locationsDao) {
         this.locationsDao = locationsDao;
     }
-
 
     @Value("${mapbox.api.key}")
     private String mapBoxKey;
 
     @GetMapping("/mapbox")
-    public String mapBox(Model model) {
-        List<Location> listAddresses = locationsDao.findAll(); //finding all address to loop through them
+    public String showMapBox(Model model) {
         model.addAttribute("mapBoxKey", mapBoxKey);
-
-        for (Location listAddress : listAddresses) {
-            String address = listAddress.addressToString();
-            model.addAttribute("address", address); //adding address to our page
-            System.out.println(address);
-        }
-
         return "reviews/index";
     }
 
-
-
+    // Adding json object to JS
+    @RequestMapping(value = "/mapbox.json", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public List<Location> userLocations() {
+        return locationsDao.findAll();
+    }
 }
