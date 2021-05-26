73b2c0c5afbbe06046f8743325ed6ad0007e7cf8
Colin Warner <colin.x.warner@gmail.com>
2021-05-26 09:55:40 -0500

    Moving admin logic to AdminController for cleaner code in user controller

--- src/main/java/com/capstone/gogreen/controllers/AdminController.java
+++ src/main/java/com/capstone/gogreen/controllers/AdminController.java
@@ -1,42 +1,51 @@
-//package com.capstone.gogreen.controllers;
-//
-//import com.capstone.gogreen.models.Job;
-//import com.capstone.gogreen.models.User;
-//import com.capstone.gogreen.repositories.*;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.validation.Errors;
-//import org.springframework.web.bind.annotation.*;
-//
-//import javax.validation.Valid;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.validation.BindingResult;
-//import org.springframework.validation.annotation.Validated;
-//import org.springframework.web.bind.annotation.*;
-//
-//import javax.validation.Valid;
-//import java.util.Arrays;
-//import java.util.List;
-//import java.util.Set;
-//
-//
-//@Controller
-//public class AdminController {
-//
-//    @Value("${mapbox.api.key}")
-//    private String mapBoxKey;
-//
-//    private final UserRepository usersDao;
-//    private final JobRepository jobsDao;
-//    private final PasswordEncoder passwordEncoder;
-//    private final LocationRepository locationsDao;
-//    private final ImageRepository imagesDao;
-//
+package com.capstone.gogreen.controllers;
+
+import com.capstone.gogreen.models.Job;
+import com.capstone.gogreen.models.User;
+import com.capstone.gogreen.repositories.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+
+@Controller
+public class AdminController {
+
+    @Value("${mapbox.api.key}")
+    private String mapBoxKey;
+
+    private final UserRepository usersDao;
+    private final JobRepository jobsDao;
+    private final PasswordEncoder passwordEncoder;
+    private final LocationRepository locationsDao;
+    private final ImageRepository imagesDao;
+
+    public AdminController(UserRepository usersDao, JobRepository jobsDao, PasswordEncoder passwordEncoder, LocationRepository locationsDao, ImageRepository imagesDao) {
+        this.usersDao = usersDao;
+        this.jobsDao = jobsDao;
+        this.passwordEncoder = passwordEncoder;
+        this.locationsDao = locationsDao;
+        this.imagesDao = imagesDao;
+    }
+
+//    @Autowired
 //    public AdminController(UserRepository usersDao, JobRepository jobsDao, PasswordEncoder passwordEncoder, LocationRepository locationsDao, ImageRepository imagesDao) {
 //        this.usersDao = usersDao;
 //        this.jobsDao = jobsDao;
@@ -44,41 +53,32 @@
 //        this.locationsDao = locationsDao;
 //        this.imagesDao = imagesDao;
 //    }
-//
-////    @Autowired
-////    public AdminController(UserRepository usersDao, JobRepository jobsDao, PasswordEncoder passwordEncoder, LocationRepository locationsDao, ImageRepository imagesDao) {
-////        this.usersDao = usersDao;
-////        this.jobsDao = jobsDao;
-////        this.passwordEncoder = passwordEncoder;
-////        this.locationsDao = locationsDao;
-////        this.imagesDao = imagesDao;
-////    }
-//
-//    @GetMapping("/admin/users")
-//    public String adminEditUser(Model model) {
-//        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal(); //Getting logged in user
-//        usersDao.getOne(loggedInUser.getId()); //getting that user by id of logged in user
-//        model.addAttribute("user", usersDao.getOne(loggedInUser.getId())); //adding that user object
-//        boolean isAdmin = usersDao.getOne(loggedInUser.getId()).getIsAdmin(); //Getting User according to logged in user and checking isAdmin row
-//        System.out.println(isAdmin); // Testing hibernate return in stack trace
-//        // Logic to redirect based off of isAdmin row from User table in db
-//        if (isAdmin) {
-//            return "admin/users";
+
+    @GetMapping("/admin/users")
+    public String adminEditUser(Model model) {
+        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal(); //Getting logged in user
+        usersDao.getOne(loggedInUser.getId()); //getting that user by id of logged in user
+        model.addAttribute("user", usersDao.getOne(loggedInUser.getId())); //adding that user object
+        boolean isAdmin = usersDao.getOne(loggedInUser.getId()).getIsAdmin(); //Getting User according to logged in user and checking isAdmin row
+        System.out.println(isAdmin); // Testing hibernate return in stack trace
+        // Logic to redirect based off of isAdmin row from User table in db
+        if (isAdmin) {
+            return "admin/users";
 //        } else {
 //            return "error/permissions";
 //        }
-//    }
-//
-//
-//    @GetMapping("/edit/{id}")
-//    @ResponseBody
-//    public User getUser (@PathVariable("id") Long id) {
-//        return usersDao.getOne(id);
-//    }
-//
-//    @GetMapping("/delete/{id}")
-//    @ResponseBody
-//    public User deleteUser (@PathVariable("id") Long id) {
-//        return usersDao.getOne(id);
-//    }
+    }
 
+
+    @GetMapping("/edit/{id}")
+    @ResponseBody
+    public User getUser(@PathVariable("id") Long id) {
+        return usersDao.getOne(id);
+    }
+
+    @GetMapping("/delete/{id}")
+    @ResponseBody
+    public User deleteUser(@PathVariable("id") Long id) {
+        return usersDao.getOne(id);
+    }
+}
--- src/main/java/com/capstone/gogreen/controllers/UserController.java
+++ src/main/java/com/capstone/gogreen/controllers/UserController.java
@@ -33,7 +33,6 @@ public class UserController {
         this.imagesDao = imagesDao;
     }
 
-
     @GetMapping("/dashboard")
     public String showUserDashboard(Model model, @ModelAttribute Job job) {
         User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -57,22 +56,6 @@ public class UserController {
         model.addAttribute("user", usersDao.getOne(loggedInUser.getId())); //adding that user object
         return "users/edit-user";
     }
-
-//    //Admin edit user information getMapping
-//    @GetMapping("/admin/users")
-//    public String adminEditUser(Model model) {
-//        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal(); //Getting logged in user
-//        usersDao.getOne(loggedInUser.getId()); //getting that user by id of logged in user
-//        model.addAttribute("user", usersDao.getOne(loggedInUser.getId())); //adding that user object
-//        boolean isAdmin = usersDao.getOne(loggedInUser.getId()).getIsAdmin(); //Getting User according to logged in user and checking isAdmin row
-//        System.out.println(isAdmin); // Testing hibernate return in stack trace
-//        // Logic to redirect based off of isAdmin row from User table in db
-//        if (isAdmin) {
-//            return "admin/users";
-//        } else {
-//            return "error/permissions";
-//        }
-//    }
 
     //Edit user information postMapping
     @PostMapping("/edit-user")
@@ -123,55 +106,6 @@ public class UserController {
         return "redirect:/dashboard";
     }
 
-    ///////////////////////////////////////////////////////
-    @PostMapping("/admin/users")
-    public String adminEditUser(@Valid @ModelAttribute User user,//Valid attribute is for validation
-                                @RequestParam(name = "password") String password,
-                                @RequestParam(name = "username") String username,
-                                @RequestParam(name = "email") String email,
-                                @RequestParam(name = "confirm") String confirm,
-//                       @RequestParam(name = "is_admin") Boolean isAdmin,
-                                Errors validation,
-                                Model model) { //errors validation also needed for validation
-        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal(); //Getting logged in user
-        User userId = usersDao.getOne(loggedInUser.getId());
-        boolean isAdmin = usersDao.getOne(loggedInUser.getId()).getIsAdmin();
-        String hash = passwordEncoder.encode(password);
 
-        // checking to make sure password and confirm password match
-        if (!user.getPassword().equals(confirm)) {
-            validation.rejectValue(
-                    "password",
-                    "user.password",
-                    "Passwords do not match"
-            );
-        }
-
-        //Check if username and email is match with some other records from our database
-        if (validation.hasErrors()) {
-            model.addAttribute("errors", validation);
-            model.addAttribute("user", user);
-            return "admin/users";
-        } else if (usersDao.findByUsername(user.getUsername()) != null && usersDao.findByEmail(user.getEmail()) != null) {
-            model.addAttribute("username", user.getUsername());
-            model.addAttribute("email", user.getEmail());
-            return "admin/dashboard";
-        } else if (usersDao.findByUsername(user.getUsername()) != null) {
-            model.addAttribute("username", user.getUsername());
-            return "admin/dashboard";
-        } else if (usersDao.findByEmail(user.getEmail()) != null) {
-            model.addAttribute("email", user.getEmail());
-            return "admin/dashboard";
-        }
-
-        //setting the username email and password
-        userId.setPassword(hash);
-        userId.setUsername(username);
-        userId.setEmail(email);
-
-        //saving those changes to the user
-        usersDao.save(userId);
-        return "redirect:/admin/dashboard";
-    }
 }
 
