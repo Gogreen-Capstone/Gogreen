35d076b93be6ef07d988b1ecd47399308f236514
Yasin Mogultay <yasin.mglty@gmail.com>
2021-05-28 15:39:42 -0500

    post pulling commit

--- src/main/java/com/capstone/gogreen/controllers/HomeController.java
+++ src/main/java/com/capstone/gogreen/controllers/HomeController.java
@@ -1,13 +1,16 @@
 package com.capstone.gogreen.controllers;
 
 import com.capstone.gogreen.models.Job;
+import com.capstone.gogreen.models.User;
 import com.capstone.gogreen.repositories.ImageRepository;
 import com.capstone.gogreen.repositories.JobRepository;
 import com.capstone.gogreen.repositories.UserRepository;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
